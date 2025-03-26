4491d04b64046a6ea0de0214a374a8439f415807
Scott Urbanski <scotturbanski@Scotts-MacBook-Air.local>
2025-03-26 10:32:06 -0500

    Created rock explosion.

---
+++ main.gd
@@ -0,0 +1,31 @@
+extends Node
+
+@export var rock_scene : PackedScene
+
+var screensize = Vector2.ZERO
+
+func _ready():
+	screensize = get_viewport().get_visible_rect().size
+	for i in 3:
+		spawn_rock(3)
+		
+func spawn_rock(size, pos=null, vel=null):
+	if pos == null:
+		$RockPath/RockSpawn.progress = randi()
+		pos = $RockPath/RockSpawn.position
+	if vel == null:
+		vel = Vector2.RIGHT.rotated(randf_range(0, TAU)) * randf_range(50,125)
+	var r = rock_scene.instantiate()
+	r.screensize = screensize
+	r.start(pos, vel, size)
+	call_deferred("add_child", r)	
+	r.exploded.connect(self._on_rock_exploded)
+	
+func _on_rock_exploded(size, radius, pos, vel):
+	if size <= 1:
+		return
+	for offset in [-1,1]:
+		var dir = $Player.position.direction_to(pos).orthogonal() * offset
+		var neewpos = pos + dir * radius
+		var newvel = dir * vel.length() * 1.1
+		spawn_rock(size - 1, neewpos, newvel)	
--- rock.gd
+++ rock.gd
@@ -1,5 +1,7 @@
 extends RigidBody2D
 
+signal exploded
+
 var screensize = Vector2.ZERO
 var size
 var radius
@@ -11,4 +13,26 @@ func start(_position, _velocity, _size):
 	mass = 1.5 * size
 	$Sprite2D.scale = Vector2.ONE * scale_factor * size
 	radius = int($Sprite2D.texture.get_size().x / 2 * $Sprite2D.scale.x)
+	var shape = CircleShape2D.new()
+	shape.radius = radius
+	$CollisionShape2D.shape = shape
+	linear_velocity = _velocity
+	angular_velocity = randf_range(-PI, PI)
+	$Explosion.scale = Vector2.ONE * .75 * size
 	
+func _integrate_forces(physics_state):
+	var xform = physics_state.transform
+	xform.origin.x = wrapf(xform.origin.x, 0 - radius, screensize.x + radius)
+	xform.origin.y = wrapf(xform.origin.y, 0 - radius, screensize.y + radius)	
+	physics_state.transform = xform
+	
+func explode():
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sprite2D.hide()
+	$Explosion/AnimationPlayer.play("explosion")
+	$Explosion.show()
+	exploded.emit(size, radius, position, linear_velocity)
+	linear_velocity = Vector2.ZERO
+	angular_velocity = 0
+	await $Explosion/AnimationPlayer.animation_finished
+	queue_free()	
